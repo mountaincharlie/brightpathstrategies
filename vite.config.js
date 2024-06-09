6401ec4c0057d9588be32e443b3258baa3f20898
Charlie <ciharland@hotmail.co.uk>
2024-06-09 16:45:36 +0100

    tidy

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // // Determine the base URL dynamically based on the environment variable
-const baseURL = process.env.NODE_ENV === 'production' ? 'https://mountaincharlie.github.io/brightpathstrategies/' : '/';
-console.log('vite config mode: ', baseURL)
+// const baseURL = process.env.NODE_ENV === 'production' ? 'https://mountaincharlie.github.io/brightpathstrategies/' : '/';
+// console.log('vite config mode: ', baseURL)
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: baseURL,
+  base: '/brightpathstrategies/',
 });
