15fcaa68294799ca3d3b0d351a8c19ba967aba68
Charlie <ciharland@hotmail.co.uk>
2024-06-09 14:43:10 +0100

    try fix routing error

--- bright-path-strategies/vite.config.js
+++ bright-path-strategies/vite.config.js
@@ -9,13 +9,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Determine the base URL dynamically based on the environment variable
-const baseURL = process.env.NODE_ENV === 'production' ? 'https://mountaincharlie.github.io/brightpathstrategies/' : '/';
-console.log('vite config mode: ', process.env.NODE_ENV)
+// // Determine the base URL dynamically based on the environment variable
+// const baseURL = process.env.NODE_ENV === 'production' ? 'https://mountaincharlie.github.io/brightpathstrategies/' : '/';
+// console.log('vite config mode: ', process.env.NODE_ENV)
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: baseURL,
+  // base: baseURL,
 });
