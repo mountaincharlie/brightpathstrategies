50a0fe849b57a5916a00ee39802b10040828b1d7
Charlie <ciharland@hotmail.co.uk>
2024-06-09 13:34:39 +0100

    devops: try fix routing error

--- bright-path-strategies/vite.config.js
+++ bright-path-strategies/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: mode === 'production' ? '/bright-path-strategies/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/brightpathstrategies/' : '/',
 });
