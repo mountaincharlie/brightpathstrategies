b7c2dcfe9424eb27e5cd3fa2bce30123ce44134b
Charlie <ciharland@hotmail.co.uk>
2024-06-09 15:37:29 +0100

    fix: incorrect install sass location

--- bright-path-strategies/vite.config.js
+++ bright-path-strategies/vite.config.js
@@ -17,5 +17,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: baseURL,
+  base: process.env.VITE_APP_BASE_URL,
 });
